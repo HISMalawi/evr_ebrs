fee331ff9524cbc184da4a73e7060b51bcd2410e
jmziya <jacobmziya@gmail.com>
2018-03-29 12:20:55 +0200

    Defining search results table in 2 columns.

--- app/assets/javascripts/itable.js
+++ app/assets/javascripts/itable.js
@@ -26,88 +26,79 @@ iTable = {
 		caption.innerHTML = (number_of_records===1?number_of_records+' record found':number_of_records + ' records found');
 		table.appendChild(caption);
 
-		var thr = document.createElement("tr");
-
-		var thd1 = document.createElement("th");
-		thd1.innerHTML = 'Index';
-		thr.appendChild(thd1);
-
-		var thd2 = document.createElement("th");
-		thd2.innerHTML = 'First Name';
-		thr.appendChild(thd2);
-
-		var thd3 = document.createElement("th");
-		thd3.innerHTML = 'Last Name';
-		thr.appendChild(thd3);
-
-		var thd4 = document.createElement("th");
-		thd4.innerHTML = 'Gender';
-		thr.appendChild(thd4);
-
-		var thd5 = document.createElement("th");
-		thd5.innerHTML = 'Birthdate';
-		thr.appendChild(thd5);
-
-		var thd6 = document.createElement("th");
-		thd6.innerHTML = 'Home District';
-		thr.appendChild(thd6);
-
-		var thd7 = document.createElement("th");
-		thd7.innerHTML = 'Home TA';
-		thr.appendChild(thd7);
-
-		var thd8 = document.createElement("th");
-		thd8.innerHTML = 'Home Village';
-		thr.appendChild(thd8);
-
-		table.appendChild(thr);
-
 		for(var key in hash){
-			var tr  = document.createElement("tr");
-			tr.style.left = '5px';
-			tr.id = hash[0]['first_name'] + '_' + hash[0]['last_name'] + '_' + key;
-			tr.onclick = function() {
+
+			// Create Main Row for Record
+			var person_row = document.createElement("tr");
+			table.appendChild(person_row);
+
+			// Create Column 1 to hold Table Headers
+			var person_header_td = document.createElement("td");
+			person_row.appendChild(person_header_td);
+
+			var person_header_td_row = document.createElement("tr");
+			person_header_td.appendChild(person_header_td_row);
+
+			var person_header_first_name = document.createElement("td");
+			person_header_first_name.innerHTML = 'First Name';
+			person_header_td_row.appendChild(person_header_first_name);
+
+			var person_header_last_name = document.createElement("td");
+			person_header_last_name.innerHTML = 'Last Name';
+			person_header_td_row.appendChild(person_header_last_name);
+
+			// Create column 2 to hold Table Description
+			var person_details_td = document.createElement("td");
+			person_row.appendChild(person_details_td);
+
+			var person_details_td_row = document.createElement("td");
+			person_details_td.appendChild(person_details_td_row);
+
+			var person_details_first_name = document.createElement("td");
+			person_details_first_name.innerHTML = hash[0]['first_name'];
+			person_details_td.appendChild(person_details_first_name);
+
+			var person_details_last_name = document.createElement("td");
+			person_details_last_name.innerHTML = hash[0]['last_name'];
+			person_details_td.appendChild(person_details_last_name);
+
+			var person_details_gender = document.createElement("td");
+			person_details_gender.innerHTML = hash[0]['gender'];
+			person_details_td.appendChild(person_details_gender);
+
+			var person_details_birthdate = document.createElement("td");
+			person_details_birthdate.innerHTML = hash[0]['birth_date'];
+			person_details_td.appendChild(person_details_birthdate);
+
+			var person_details_home_address = document.createElement("td");
+			person_details_home_address.innerHTML = hash[0]['home_village'] + ',' + hash[0]['home_ta'] + ',' + hash[0]['home_district'];
+			person_details_td.appendChild(person_details_home_address);
+
+			var person_details_current_address = document.createElement("td");
+			person_details_current_address.innerHTML = hash[0]['current_village'] + ',' + hash[0]['current_ta'] + ',' + hash[0]['current_district'];
+			person_details_td.appendChild(person_details_current_address);
+
+			// Create column 3 to hold Action Buttons
+			var person_action_td = document.createElement("td");
+			person_action_td.innerHTML = 'action column';
+			person_row.appendChild(person_action_td);
+
+			var person_action_td_row = document.createElement("tr");
+			person_action_td.appendChild(person_action_td_row);
+
+			var person_action_td_row_td = document.createElement("td");
+			person_action_td_row.appendChild(person_action_td_row_td);
+
+			var person_action_select_button = document.createElement("a");
+			person_action_select_button.innerHTML = 'SELECT';
+			person_action_select_button.onclick = function() {
 				getDataAndStore(hash);
 			};
+			person_action_td_row_td.appendChild(person_action_select_button);
 
-			var td1 = document.createElement("td");
-			td1.innerHTML  = parseInt(key) + 1;
-			tr.appendChild(td1);
-
-			var td2 = document.createElement("td");
-			td2.innerHTML  = hash[0]['first_name'];
-			tr.appendChild(td2);
-
-			var td3 = document.createElement("td");
-			td3.innerHTML  = hash[0]['last_name'];
-			tr.appendChild(td3);
-
-			var td4 = document.createElement("td");
-			if("hash[0]['gender'] == 'F'") {
-				var gender = 'Female';
-			}else if("hash[0]['gender'] == 'F'") {
-				var gender = 'Male';
-			}
-			td4.innerHTML  = gender;
-			tr.appendChild(td4);
-
-			var td5 = document.createElement("td");
-			td5.innerHTML  = hash[0]['birth_date'];
-			tr.appendChild(td5);
-
-			var td6 = document.createElement("td");
-			td6.innerHTML  = hash[0]['home_district'];
-			tr.appendChild(td6);
-
-			var td7 = document.createElement("td");
-			td7.innerHTML  = hash[0]['home_ta'];
-			tr.appendChild(td7);
-
-			var td8 = document.createElement("td");
-			td8.innerHTML  = hash[0]['home_village'];
-			tr.appendChild(td8);
-
-			table.appendChild(tr);
+			var person_action_select_and_edit_button = document.createElement("a");
+			person_action_select_and_edit_button.innerHTML = 'SELECT & EDIT';
+			person_action_td_row_td.appendChild(person_action_select_and_edit_button);
 		}
 		parent.append(table);
 		return table;
