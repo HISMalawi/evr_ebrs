54fbec4ebfeb3f6f28029c810e439f578d24e128
jmziya <jacobmziya@gmail.com>
2019-01-31 10:02:17 +0200

    Finalized creating from DDE via ART.

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -524,42 +524,25 @@ class PersonController < ApplicationController
         params['patient_year'] = birthdate.to_datetime.year
 
         demographics_hash = {
-            'occupation' =>"#{(params['attributes']['occupation'] rescue [])}",
-            'education_level'=>"#{(params['person']['level_of_education'] rescue [])}",
-            'religion'=>"#{(params['attributes']['religion'] rescue [])}",
-            'patient_year'=>"#{params['patient_year'] rescue []}",
-            'patient'=> {
-                'gender' =>"#{params['person']['gender']}",
-                'birthplace' =>"#{params['addresses']['address2'] rescue []}",
-                'creator' => 1,
-                'changed_by' => 1
-            },
-            'p_address'=> {
-                'identifier' => "#{params['addresses']['state_province'] rescue []}"},
-            'home_phone' => {
-                'identifier' => "#{(params['attributes']['home_phone_number'] rescue [])}"},
-            'cell_phone' => {
-                'identifier' => "#{(params['attributes']['cell_phone_number'] rescue [])}"},
-            'office_phone' => {
-                'identifier' => "#{(params['attributes']['office_phone_number'] rescue [])}"},
-            'patient_id' => '',
-            'patient_day' => "#{params['patient_day']}",
-            'patientaddress' => {'city_village' => "#{new_params['addresses']['city_village'] rescue []}"},
-            'patient_name' => {
-                'family_name' => "#{params['person']['first_name']}",
-                'given_name' => "#{params['person']['last_name']}",
-                'middle_name' => "#{params['person']['middle_name'] rescue []}",
-                'creator' => 1
-            },
-            'patient_month' => "#{params['patient_month']}",
-            'patient_age' =>{
-                'age_estimate' => "#{params['age_estimate']}"
-            },
-            'age' =>{
-                'identifier' =>''
-            },
-            'current_ta' =>{
-                'identifier' => "#{params['addresses']['county_district'] rescue []}"
+            "person" => {
+                "birth_year" => params['patient_year'],
+                "birth_month" => params['patient_month'],
+                "birth_day" => params['patient_day'],
+                "names" => {
+                    "family_name" => params["person"]["first_name"],
+                    "given_name" => params["person"]["last_name"],
+                    "middle_name" => (params["person"]["names"]["middle_name"] rescue [])
+                },
+                "gender" => params["person"]["gender"],
+                "addresses" => {
+                    "city_village" => params["person"]["mother"]["current_village"],
+                    "township_division" => (params["person"]["mother"]["current_ta"] rescue []),
+                    "state_province" => params["person"]["mother"]["current_district"],
+
+                    "neighborhood_cell" => params["person"]["mother"]["home_village"],
+                    "county_district" => params["person"]["mother"]["home_ta"],
+                    "address2" => params["person"]["mother"]["home_district"]
+                },
             }
         }
       else
@@ -614,7 +597,11 @@ class PersonController < ApplicationController
 
     remote_person_record  = JSON.parse(RestClient.post(link, demographics_hash.to_json, :content_type => "application/json"))
 
-    child_national_id = remote_person_record['person']['patient']['identifiers']['National id'].inspect
+    if SETTINGS['remote_dde_status'].to_s == "true"
+      child_national_id = remote_person_record['npid']
+    else
+      child_national_id = remote_person_record['person']['patient']['identifiers']['National id'].inspect
+    end
 
     child_id = @person.person_id
     child_identifier_type_id = PersonIdentifierType.find_by_name('Barcode Number').person_identifier_type_id
