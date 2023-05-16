e5b3906c153582c8a92cf49d9e1035b13d60f8ce
Emmanuel Mkambankhani <emmanuel.mkambankhani@gmail.com>
2023-05-16 17:40:26 +0200

    Set cronjobs for sync

---
+++ config/initializers/set_cronjobs.rb
@@ -0,0 +1,15 @@
+cron_tab_entries = `crontab -l`
+PATH=`echo $PATH`
+unless cron_tab_entries.include?("PATH")
+	path = "(crontab -l 2>/dev/null; echo 'PATH=#{PATH}') | crontab -"
+	`#{path}`
+end
+app_root = Rails.root
+entries_to_add = [		
+					"*/5 * * * * bash -l -c 'cd #{app_root} && bundle exec rails r bin/sync_background.rb'"
+				]
+entries_to_add.each do |entry|
+	next if cron_tab_entries.include?(entry)
+	new_entry = "(crontab -l 2>/dev/null; echo \"#{entry}\") | crontab -"
+	`#{new_entry}`
+end
